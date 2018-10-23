3f486d0756d44d0ba946e261ccdfa6f4e146c552
adenurhidayat <adenurhidayat101294@gmail.com>
2018-10-23 13:09:17 +0700

    new web

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});*/
+ 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {page:'Home', menuId:'home'});
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', {page:'Home', menuId:'homes'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {page:'Contact', menuId:'contact'});
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', {page:'Services', menuId:'services'});
+});
+
+router.get('/work', function(req, res, next) {
+  res.render('work', {page:'Work', menuId:'work'});
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('blog', {page:'Blog', menuId:'blog'});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {page:'About', menuId:'about'});
+});
+
+
+module.exports = router;
